b2762f538e05dcd2fe1a33fed6cb63712103b82f
loljoho <jho1290@smail.raritanval.edu>
2015-08-26 14:40:19 -0400

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,42 @@
+(function(main) {
+  'use strict';
+
+  // Pass global jQuery obj as param
+  main(window.jQuery, window, document);
+
+}(function($, window, document) {
+  'use strict';
+
+  // Locally scoped $
+
+  // Document Ready
+  // -------------------------------
+  $(function() {
+
+
+
+    // Retrieve Brand Text
+    // -------------------------------
+
+    getResume().done(function(data) {
+      $('.navbar-brand').text(data.title);
+    });
+
+  });
+
+
+
+  // Return Ajax Request Promise
+  // -------------------------------
+
+  function getResume() {
+    return $.ajax({
+      url       : "data/test.json",
+      type      : 'GET',
+      dataType  : 'json'
+    });
+  }
+
+
+
+}));
